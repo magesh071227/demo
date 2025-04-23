80079d0e2fcacc4700319d21544d16c0ccaefe40
magesh0712 <mageshp700@gmail.com>
2025-04-23 11:03:48 +0530

    Update index.php

--- cafe/index.php
+++ cafe/index.php
@@ -44,7 +44,7 @@
 			<table>
 				<tr>
 					<td bgcolor="aquamarine">
-						<div class="cursiveText">Magesh2606 bakes a rich variety of cookies. Try them all!</div>
+						<div class="cursiveText">Magesh26 bakes a rich variety of cookies. Try them all!</div>
 						<table>
 							<tr>
 								<td><img src="images/Cookies.jpg" height=auto width="300"></td>
